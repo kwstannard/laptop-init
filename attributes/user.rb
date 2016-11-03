0a675827523639bec1bdbc1180d8f2456bc24b20
Kelly Stannard <kwstannard@gmail.com>
2016-11-03 09:49:02 -0400

    move user attributes to node.json

--- attributes/user.rb
+++
@@ -1,7 +0,0 @@
-default['desktop']['user'].tap do |user|
-  user['name'] = 'kwstannard'
-  user['uid'] = '1000'
-  user['group'] = 'kwstannard'
-  user['gid'] = '1000'
-  user['home'] = '/home/kwstannard'
-end
